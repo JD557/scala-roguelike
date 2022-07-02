ca3d75ccf7690e05ea451555f8ccaf9f8bb2ccbb
Joao Costa <jdpc557@gmail.com>
2022-07-02 20:20:51 +0200

    Improve FOV

--- shared/src/main/scala/eu/joaocosta/roguelike/GameMap.scala
+++ shared/src/main/scala/eu/joaocosta/roguelike/GameMap.scala
@@ -12,23 +12,26 @@ case class GameMap(tiles: Map[(Int, Int), GameMap.Tile]) {
     def sameDirection(seedX: Int, seedY: Int, dx: Int, dy: Int): Boolean = {
       val sdx = math.signum(seedX - x)
       val sdy = math.signum(seedY - y)
-      (sdx == 0 || sdx == dx) && (sdy == 0 || sdy == dy)
+      (sdx == 0 || dx == 0 || sdx == dx) && (sdy == 0 || dy == 0 || sdy == dy)
     }
 
     @tailrec
-    def floodFill(seeds: Set[(Int, Int)], remaining: Int, acc: Set[(Int, Int)]): Set[(Int, Int)] =
-      if (remaining <= 0 || seeds.isEmpty) acc
+    def floodFill(seeds: Set[((Int, Int), Double)], acc: Set[(Int, Int)]): Set[(Int, Int)] =
+      if (seeds.isEmpty) acc
       else {
         val candidates = (for {
-          (sx, sy) <- seeds
-          dx       <- -1 to 1
-          dy       <- -1 to 1
+          ((sx, sy), remaining) <- seeds
+          dx                    <- -1 to 1
+          dy                    <- -1 to 1
           if !(dx == 0 && dy == 0) && sameDirection(sx, sy, dx, dy)
-        } yield (sx + dx, sy + dy)).toSet
-        val nextSeeds = candidates.filter(isTransparent)
-        floodFill(nextSeeds, remaining - 1, acc ++ candidates)
+          cost          = if (dx == 0 || dy == 0) 1.0 else math.sqrt(2)
+          nextRemaining = remaining - cost
+          if nextRemaining >= 0
+        } yield ((sx + dx, sy + dy), nextRemaining)).toSet
+        val nextSeeds = candidates.filter { case ((x, y), _) => isTransparent(x, y) }
+        floodFill(nextSeeds, acc ++ candidates.map(_._1))
       }
-    floodFill(Set((x, y)), range, Set((x, y)))
+    floodFill(Set(((x, y), range)), Set((x, y)))
   }
 
   def isVisibleFrom(x1: Int, y1: Int, x2: Int, y2: Int, range: Int) =
