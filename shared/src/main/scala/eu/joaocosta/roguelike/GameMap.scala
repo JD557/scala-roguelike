b9ec0decb13d54a5d45780de4b236ad4bd97b3e3
Joao Costa <jdpc557@gmail.com>
2022-07-02 11:43:52 +0200

    Improve visibility algorithm

--- shared/src/main/scala/eu/joaocosta/roguelike/GameMap.scala
+++ shared/src/main/scala/eu/joaocosta/roguelike/GameMap.scala
@@ -6,6 +6,13 @@ case class GameMap(tiles: Map[(Int, Int), GameMap.Tile]) {
   def isTransparent(x: Int, y: Int) = tiles.get((x, y)).forall(_.walkable)
 
   def visibleFrom(x: Int, y: Int, range: Int): Set[(Int, Int)] = {
+    // Don't allow the floodFill to "go back"
+    def sameDirection(seedX: Int, seedY: Int, dx: Int, dy: Int): Boolean = {
+      val sdx = math.signum(seedX - x)
+      val sdy = math.signum(seedY - y)
+      (sdx == 0 || sdx == dx) && (sdy == 0 || sdy == dy)
+    }
+
     @tailrec
     def floodFill(seeds: Set[(Int, Int)], remaining: Int, acc: Set[(Int, Int)]): Set[(Int, Int)] =
       if (remaining <= 0 || seeds.isEmpty) acc
@@ -14,6 +21,7 @@ case class GameMap(tiles: Map[(Int, Int), GameMap.Tile]) {
           (sx, sy) <- seeds
           dx       <- -1 to 1
           dy       <- -1 to 1
+          if !(dx == 0 && dy == 0) && sameDirection(sx, sy, dx, dy)
         } yield (sx + dx, sy + dy)).toSet
         val nextSeeds = candidates.filter(isTransparent)
         floodFill(nextSeeds, remaining - 1, acc ++ candidates)
